e35f511b6e8da24a8d7b7ea500789501d843ab5f
Mustafa Yılmaz <105638480+MustafaYilmaz1625@users.noreply.github.com>
2022-06-06 19:45:59 +0300

    router setup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+// pages
+import Home from "./components/Home";
+import About from "./components/About";
+import Users from "./components/Users";
 
 function App() {
   return (
@@ -20,9 +24,6 @@ function App() {
           </ul>
         </nav>
 
-        {/* A <Switch> looks through its children <Route>s and
-        renders the first one that matches the current URL. */}
-
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="about/*" element={<About />} />
@@ -31,17 +32,6 @@ function App() {
       </div>
     </Router>
   );
-}
-function Home() {
-  return <h2>Home</h2>;
-}
-
-function About() {
-  return <h2>About</h2>;
-}
-
-function Users() {
-  return <h2>Users</h2>;
 }
 
 export default App;
