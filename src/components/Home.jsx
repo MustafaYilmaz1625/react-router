6f890a02a19816ce586330ce9d585dbec6634f50
Mustafa Yılmaz <105638480+MustafaYilmaz1625@users.noreply.github.com>
2022-06-12 18:56:36 +0300

    useNavigate

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 function Home() {
-  return <div>Home</div>;
+  let navigate = useNavigate();
+  return (
+    <div>
+      <div>This is the USERS PAGE</div>
+      <button
+        style={{
+          background: "#9CCC65",
+          padding: 5,
+          marginLeft: 5,
+          border: "none",
+          borderRadius: "5px",
+        }}
+        onClick={() => {
+          navigate("/users");
+        }}
+      >
+        {""} Change to about page
+      </button>
+    </div>
+  );
 }
 
 export default Home;
